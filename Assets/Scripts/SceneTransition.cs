ad977af599fed51b3be53a3b614f856401715205
kimdoann <159505113+kimdoann@users.noreply.github.com>
2025-09-16 02:16:44 -0700

    add canvas activator script, scene change script and 1st pass in the level
    feature a canvas activator script and scene change script

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,30 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using UnityEngine.SceneManagement;
-
-public class SceneLoader : MonoBehaviour
-{
-    public void LoadGame()
-    {
-        SceneManager.LoadScene("GameScene");
-    }
-    
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-=======
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
